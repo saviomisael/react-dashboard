76c61c3a91e971f9080c176760989fb520717faa
Savio Moreira <misaelmoreira99.sm@gmail.com>
2023-07-09 15:08:11 -0300

    refactor StatBox

--- src/components/StatBox.jsx
+++ src/components/StatBox.jsx
@@ -7,8 +7,15 @@ export const StatBox = ({ title, subtitle, icon, progress, increase }) => {
   const colors = tokens(theme.palette.mode);
 
   return (
-    <Box width="100%" m="0 30px">
-      <Box display="flex" justifyContent="space-between">
+    <Box
+      width="100%"
+      m="0 30px"
+      display="grid"
+      gridTemplateColumns="repeat(12, 1fr)"
+      alignItems="center"
+      gap="5px"
+    >
+      <Box display="flex" justifyContent="space-between" gridColumn="span 6">
         {icon}
         <Typography
           variant="h4"
@@ -19,11 +26,11 @@ export const StatBox = ({ title, subtitle, icon, progress, increase }) => {
         </Typography>
       </Box>
 
-      <Box>
+      <Box gridColumn="span 6" justifySelf="end">
         <ProgressCircle progress={progress} />
       </Box>
 
-      <Box display="flex" justifyContent="space-between">
+      <Box display="flex" justifyContent="space-between" gridColumn="span 12">
         <Typography variant="h5" sx={{ color: colors.greenAccent[500] }}>
           {subtitle}
         </Typography>
